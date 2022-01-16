9d7bf10e88fe910967d334c4487679b1731b9c69
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2022-01-16 18:47:48 +0530

    Time: 19 ms (34.01%), Space: 37.7 MB (22.81%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -2,17 +2,17 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        int start = 1, end = n, result = 0;
-        while(start <= end) {
-            int mid = start + (end - start)/2;
-            if(isBadVersion(mid)) {
-                result = mid;
-                end = mid - 1;
-            } else
-                start = mid + 1;
+public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
-        return result;
+        return left;
     }
-    
 }
