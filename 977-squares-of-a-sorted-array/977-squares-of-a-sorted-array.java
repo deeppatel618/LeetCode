cfc140aaaa4f2efb25dbd4c41860494f778b8a7a
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2022-01-18 23:00:43 +0530

    Time: 2 ms (67.72%), Space: 53 MB (32.16%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,18 +1,36 @@
 class Solution {
-        public int[] sortedSquares(int[] nums) {
-        int index=0,temp;
-        for(int i=0;i< nums.length;i++)
-        {
-            index= i;
-            temp= (int) Math.pow(nums[i],2);
-            while( index > 0 && temp < nums[index-1] )
-            {
-                nums[index]=nums[index-1];
-                index--;
-            }
-            nums[index]=temp;
+       public int[] sortedSquares(int[] nums) {
+        int pos = 0;
+        int neg = 0;
+        int cindex = 0;
+        int ansIndex = 0;
+        int ans[] = new int[nums.length];
+        for(int i : nums){
+            if(i>=0)
+                break;
+            cindex++;
         }
-        return nums;
+        pos = cindex;
+        neg = cindex-1;
+        while(neg>=0 && pos < nums.length){
+            if(nums[neg]*-1 < nums[pos]){
+                ans[ansIndex++] = nums[neg]*nums[neg];
+                neg--;
+            }
+            else{
+                ans[ansIndex++] = nums[pos]*nums[pos];
+                pos++;
+            }
+        }
+        while(neg>=0){
+            ans[ansIndex++] =nums[neg]*nums[neg];
+            neg--;
+        }
+        while(pos < nums.length){
+            ans[ansIndex++] = nums[pos]*nums[pos];
+             pos++;
+        }
+        return ans;   
     }
 
 }
