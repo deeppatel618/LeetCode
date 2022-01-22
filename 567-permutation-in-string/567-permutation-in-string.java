f1a787037b00ef807ef204dadd23203d3800f683
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2022-01-22 23:40:14 +0530

    Time: 10 ms (61.49%), Space: 39.6 MB (51.69%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,32 +1,24 @@
 class Solution {
-    public boolean checkInclusion(String s1, String s2) {
+  public boolean checkInclusion(String s1, String s2) {
         int[] arr=new int[26];
-        int[] temp;
-        int num,steps=0;
-        boolean reset=false;
-        for (int i = 0; i < s1.length(); i++) {
+        int[] temp=new int[26];
+        int patternLength=s1.length();
+        if(patternLength> s2.length()) return false;
+        for (int i = 0; i < patternLength; i++) {
             arr[ s1.charAt(i) - 'a']+=1;
+            temp[s2.charAt(i) - 'a']+=1;
         }
-        temp=arr.clone();
-        for (int i = 0; i < s2.length(); i++) {
-            num=s2.charAt(i)-'a';
-            if( temp[num]>0)
-            {
-                temp[num]--;
-                steps+=1;
-                reset=true;
-                if(steps==s1.length()) return true;
+        for (int i = 0; i < s2.length()-patternLength; i++) {
+            if(Arrays.equals(arr,temp)){
+                return true;
             }
-            else
-            {
-                if(reset)
-                {
-                    reset=!reset;
-                    i=i-steps;
-                    steps=0;
-                    temp=arr.clone();
-                }
+            else{
+                temp[s2.charAt(i) - 'a']-=1;
+                temp[s2.charAt(i+patternLength) - 'a']+=1;
             }
+        }
+      if(Arrays.equals(arr,temp)){
+            return true;
         }
         return false;
     }
