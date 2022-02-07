e78880b4d52d3f2781c631aed30d0102f6f0da3e
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2022-02-07 21:47:16 +0530

    Time: 17 ms (30.55%), Space: 45 MB (10.65%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -2,12 +2,11 @@ class Solution {
     public boolean isPalindrome(int x) {
         boolean flag=true;
         String str=String.valueOf(x);
-        char[] a=str.toCharArray();
         int starting=0;
-        int ending=a.length-1;
+        int ending=str.length()-1;
         while(starting<=ending)
         {
-            if(a[starting]!=a[ending]){
+            if(str.charAt(starting)!=str.charAt(ending)){
                 return false;
             }
             starting+=1;
