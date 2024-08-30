5735dd95ae2d7ca9d1c1de5ed0713b1748222ec8
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2024-08-29 21:05:58 -0700

    Time: 2 ms (42.16%), Space: 7.4 MB (86.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int maxM,maxN;
-    long long pathTracker(int i, int j) {
+    long long pathTracker(int i, int j,vector<vector<int>> &dp) {
         if (i == maxM - 1 && j == maxN - 1) {
             return 1; // Return 1 when reaching the end of the grid
         }
         if (i >= maxM || j >= maxN) {
-            return 0; // Return 0 if out of bounds
+            return 0; 
         }
-        
-        // Recursively count paths from the current position to the right and down
-        return pathTracker(i + 1, j) + pathTracker(i, j + 1);
+        if(dp[i][j] != -1) return dp[i][j];
+    
+        else return dp[i][j] =pathTracker(i + 1, j,dp) + pathTracker(i, j + 1,dp);
+    }
+    long long totalNumberOfPathFinder(int n, int r)
+    {
+        long long counter =1;
+        for(int i = 0; i < r; i++ )
+        {
+            counter *= n;
+            counter/=i+1;
+            n--;
+        }
+        return counter;
     }
     int uniquePaths(int m, int n) {
         maxM = m;
         maxN = n;
-        return pathTracker(0,0);
+        // vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        
+        return totalNumberOfPathFinder(m+n-2,n-1<m-1?n-1:m-1);
     }
 };
