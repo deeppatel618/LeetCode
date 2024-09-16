16c21aa8de39811b160374ed70b7c704a3b89c8b
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2024-09-15 21:03:56 -0700

    Time: 7 ms (70.85%), Space: 21.4 MB (60.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,46 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size() == 1) return 1;
-        int low=0,high=1;
-        while(high!=nums.size())
+        int i = 0;
+        for(int j = 1; j < nums.size() ; j++)
         {
-            if(nums[low]==nums[high])
+            if(nums[i] != nums[j])
             {
-                nums[high] = -101;
-                high++;
-            }
-            else
-            {
-                low = high;
-                high++;
-            } 
-        }
-        low = 0;
-        high = 1; 
-        while(high != nums.size()) {
-            while(low < nums.size() && nums[low] != -101) {
-                low++; 
-            }
-            while(high < nums.size() && nums[high] == -101) {
-                high++;
-            }
-            // cout << low << "," << high << endl;
-            if(low >= nums.size() || high >= nums.size()) {
-                break;
-            } else {
-                if(low<high)
-                {
-                    nums[low] = nums[high];
-                    nums[high] = -101;
-                }
-                else
-                {
-                    high++;
-                }
+                i++;
+                nums[i] = nums[j];
+                
             }
         }
-        
-        return low;
+        return i+1;
     }
 };
