1f2ff16cc56bdc4d02903aa068ef53af1442454f
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2024-09-29 18:29:13 -0700

    Time: 4 ms (56.75%), Space: 12.9 MB (42.16%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void combinationSumTracker(int index,int sum,vector<vector<int>> &ans,vector<int>& nums, int target,vector<int>& temp)
+    {
+        if(sum > target || index > nums.size()) return;
+        
+        if(sum == target) {
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=index; i<nums.size();i++)
+        {
+            if(i>index && nums[i] == nums[i-1]) continue;
+        temp.push_back(nums[i]);
+        combinationSumTracker(i+1,sum+nums[i],ans,nums,target,temp);
+        temp.pop_back();
+        }
+
+
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> resultTracker;
+        vector<vector<int>> result;
+        sort(candidates.begin(),candidates.end());
+        combinationSumTracker(0,0,result,candidates,target,resultTracker);
+        return result;
+    }
+};
