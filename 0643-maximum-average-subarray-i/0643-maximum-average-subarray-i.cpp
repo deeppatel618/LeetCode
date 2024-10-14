1d8c41c661d5d6056f8ee3a7919163eaa6b5809d
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2024-10-13 23:12:29 -0700

    Time: 110 ms (93.05%), Space: 112.7 MB (74.63%) - LeetHub

---
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int right = 0;
+        double sum = 0;
+        for(int i=0; i<k;i++)
+        {
+            sum+=nums[i];
+        }
+        double ans = sum;
+        for(int i=k; i<nums.size();i++)
+        {
+            sum+=nums[i]-nums[i-k];
+            ans = max(ans,sum);
+        }
+        return ans/k;
+    }
+};
