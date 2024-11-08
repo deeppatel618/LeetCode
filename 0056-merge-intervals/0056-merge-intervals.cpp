6f34595c09f345e6aa268c89d0bb8d96334f90a2
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2024-11-07 22:38:08 -0800

    Time: 11 ms (18.01%), Space: 24.1 MB (17.17%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
-    void sorting(vector<vector<int>>& intervals)
-    {
-        std::sort(intervals.begin(),intervals.end(),[](const std::vector<int>&a,   const std::vector<int>& b) 
-        { return a[0] < b[0]; }
-        );
-    }
+    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sorting(intervals);
+        sort(intervals.begin(),intervals.end());
         vector<vector<int>> ans;
-        int start=intervals[0][0];
-        int end=intervals[0][1];
-        for(int i = 0; i < intervals.size()-1 ;i++)
-        {
-            if(end >= intervals[i+1][0])
+        ans.push_back(intervals[0]);
+        for(auto it:intervals){
+            if(ans.back()[1] >= it[0])
             {
-                end = end>intervals[i+1][1]? end:intervals[i+1][1];
+                ans.back()[1] = max( ans.back()[1],it[1]);
             }
             else
             {
-                vector<int> temp={start,end};
-                start = intervals[i+1][0];
-                end = intervals[i+1][1];
-                ans.push_back(temp);
+                ans.push_back(it);
             }
         }
-        vector<int> temp={start,end};
-        ans.push_back(temp);
+        
+        // ans.push_back(temp);
         return ans;
     }
 };
