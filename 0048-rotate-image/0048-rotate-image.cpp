3c6800f10501c43338c7d0306715781fd915ef3c
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2024-11-09 16:29:33 -0800

    Time: 0 ms (100%), Space: 8.8 MB (96.57%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,20 +1,57 @@
 class Solution {
 public:
-    void rotate(vector<vector<int>>& matrix) {
-        for(int i = 0; i<matrix[0].size(); i++ )
+    void transpose(vector<vector<int>> &matrix)
+    {
+        for(int i = 0;i<matrix.size();i++)
         {
-            for(int j = 0; j < i; j++)
+            for(int j = i;j<matrix[0].size();j++)
             {
-                if(i!=j)
-                {
-                    swap(matrix[i][j],matrix[j][i]);
-                }
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i = 0; i< matrix[0].size();i++)
+    }
+    void reverse(vector<vector<int>>& matrix)
+    {
+        for(int i = 0;i<matrix.size();i++)
         {
-            reverse(matrix[i].begin(), matrix[i].end());
+            int n = matrix.size();
+            for(int j = 0;j<matrix[0].size()/2;j++)
+            {
+                swap(matrix[i][j],matrix[i][n-j-1]);
+            }
         }
-        
+    }
+    void rotate(vector<vector<int>>& matrix) {
+
+        // //Transpose and reverse the row.
+        // for(int i = 0; i < matrix[0].size(); i++ )
+        // {
+        //     for(int j = i; j < matrix[0].size(); j++)
+        //     {
+        //         if(i!=j)
+        //         {
+        //             swap(matrix[i][j],matrix[j][i]);
+        //         }
+        //     }
+        // }
+        // for(int i = 0; i< matrix[0].size();i++)
+        // {
+        //     reverse(matrix[i].begin(), matrix[i].end());
+        // }
+
+        // int n = matrix.size();
+        // for(int i = 0; i< (n+1)/2;i++)
+        // {
+        //     for(int j = 0; j < n/2; j++)
+        //     {
+        //         int temp = matrix[n-1-j][i];
+        //         matrix[n-1-j][i] = matrix[n-1-i][n-j-1];
+        //         matrix[n-1-i][n-j-1] = matrix[j][n-1-i];
+        //         matrix[j][n-1-i] = matrix[i][j];
+        //         matrix[i][j] = temp;
+        //     }
+        // } 
+        transpose(matrix);
+        reverse(matrix);
     }
 };
