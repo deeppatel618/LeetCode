8f1fdce01715a96b98268aff9d33454264d834aa
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2024-12-31 00:44:53 -0800

    Time: 44 ms (36.79%), Space: 105.9 MB (25.24%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        long total = 0;
+        int left = 0;
+        int maxFreq = 1;
+        for(int right = 0; right<nums.size(); right++)
+        {
+            long curr =nums[right];
+            total+=curr;
+            if((curr * (right - left +1)) <= (total+k)){
+                maxFreq = max(maxFreq , right - left +1 );
+            }
+            else{
+                while((curr * (right - left +1)) > (total+k) && left<=right)
+                {
+                    total -= nums[left];
+                    left++;
+                }
+            }
+                
+        }
+        return maxFreq;
+    }
+};
