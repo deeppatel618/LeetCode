5494ec37d64fa95b7c24036e42c6cdfb340ace45
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2025-01-17 19:05:27 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
+    // Cummilative sum and store it in map.
+    // Use a hash map (mp) to store how many times each cumulative sum has occurred.
+    // Logic - explain using an array which stores cumulative sum of and find out difference between 2 indices
     int subarraySum(vector<int>& nums, int k) {
+
         unordered_map<int,int> mp(nums.size());
         mp[0] = 1;
         int sum = 0;
         int ans = 0;
         for(int i = 0; i<nums.size();i++)
         {
-            sum+=nums[i];
+            sum += nums[i];
             int key = sum -k;
             if(mp.find(key) !=mp.end())
             {
                 ans+=mp.find(key)->second;
             }
             mp[sum]++;
+            for(auto it:mp)
+            {
+                cout<<it.first<<" -> "<<it.second<<endl;
+            }
+            cout<<"Done for now"<<endl;
         }
         return ans;
-
+        //O(n) O(n)
     }
 };
