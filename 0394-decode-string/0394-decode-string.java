696f9c7f337e6e39a684afcf718805914b1aa481
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2025-03-19 10:52:25 -0700

    Time: 1 ms (72.21%), Space: 41.7 MB (54.36%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,37 +1,28 @@
+
 class Solution {
     public String decodeString(String s) {
-        
-        Stack<Character> st = new Stack<>();
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) == ']'){
-                List<Character> decodedString = new ArrayList<>();
-                while(st.peek() != '['){
-                    decodedString.add(st.pop());
-                }
-                st.pop();
-                int base = 1;
-                int k = 0;
-                while(!st.isEmpty() && Character.isDigit(st.peek())){
-                    k = k + ((st.pop() - '0') * base);
-                    base = base * 10;
-                }
+        Stack<String> stack = new Stack<>();  // Stack to store string components
+        StringBuilder currentString = new StringBuilder();
+        int k = 0;
 
-                while(k > 0){
-                    for(int j = decodedString.size() - 1; j >= 0; j--){
-                        st.push(decodedString.get(j));
-                    }
-                    k--;
-                }
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');  // Build the full number for repetition
+            } else if (ch == '[') {
+                // Push current string and k onto stack
+                stack.push(currentString.toString());  
+                stack.push(String.valueOf(k));
+                currentString = new StringBuilder(); // Reset for new substring
+                k = 0;  // Reset repeat count
+            } else if (ch == ']') {
+                int repeatCount = Integer.parseInt(stack.pop());  // Get stored repeat count
+                String previousString = stack.pop();  // Get previous string from stack
+                String repeatedString = currentString.toString().repeat(repeatCount);
+                currentString = new StringBuilder(previousString + repeatedString);  // Append expanded substring
             } else {
-                st.push(s.charAt(i));
-            }   
+                currentString.append(ch);  // Build substring
+            }
         }
-
-        StringBuilder res = new StringBuilder();
-        for(int i = st.size() - 1; i >= 0; i--){
-            res.append(st.pop());
-        }
-
-        return res.reverse().toString();
+        return currentString.toString();
     }
 }
