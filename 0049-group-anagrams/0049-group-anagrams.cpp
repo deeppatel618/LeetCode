31b9857e15f9972de8a3d5355d4c4093ba698445
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2025-03-26 19:17:15 -0700

    Time: 21 ms (37.43%), Space: 24 MB (93.72%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,35 +1,5 @@
 class Solution {
 public:
-    // Instead of sorting the array we can count the characters and create a string like #countA#countB
-    vector<vector<string>> groupAnagrams(vector<string>& wordlist) {
-        if(wordlist.size() == 0) return {{}};
-
-        unordered_map<string,vector<string>> mappedData;
-        int count[26];
-        for(string word: wordlist){
-            fill(begin(count),end(count),0);
-            for(char c: word){
-                count[c - 'a']++;// count the frequency.
-            }
-            string key = "";
-            for(int i = 0; i < 26; i++){
-                key += "#" + to_string(count[i]);
-            }
-            mappedData[key].push_back(word);
-        }
-        vector<vector<string>> result;
-        for(auto it:mappedData){
-            result.push_back(it.second);
-        }
-        return result;
-    }
-};
-
-
-
-
-class Solution2 {
-public:
     vector<vector<string>> groupAnagrams(vector<string>& wordlist) {
       vector<vector<string>> result;
     unordered_map<string, vector<string>> mp;
@@ -48,12 +18,6 @@ public:
     }
 
     return result;
-
-    //Time Complexity: O(NKlogK), where N is the length of strs, and K is the maximum length of a string in strs. The outer loop has complexity O(N) as we iterate through each string. Then, we sort each string in O(KlogK) time.
-
-//Space Complexity: O(NK), the total information content stored in ans.
-        
-
     }
 };
 
