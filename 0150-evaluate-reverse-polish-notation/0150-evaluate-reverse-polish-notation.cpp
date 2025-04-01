ea79f1ad92d8497c2bc9c3c4d3933803d48fdb54
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2025-04-01 11:51:23 -0700

    Time: 1 ms (53.92%), Space: 17 MB (86.44%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Insert all the integers into the stack, if you encounter any symbol then fetch top 2 integers and perform the operation.
+    // After the opeation, insert the answer into stack.
     int evalRPN(vector<string>& tokes) {
         stack<int> st;
         for(int i = 0; i<tokes.size();i++){
@@ -18,5 +20,6 @@ public:
             }
         }
         return st.top();
+        //O(n) O(n).
     }
 };
