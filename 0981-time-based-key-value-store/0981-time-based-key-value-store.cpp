7ea313bb0e69efe13b2daad35275568b96342385
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2025-04-04 10:58:01 -0700

    Time: 46 ms (73.47%), Space: 136.5 MB (77.02%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,20 +1,25 @@
 class TimeMap {
 public:
+    // Binary search
+
+    // Clarification on the problem - If there is no exact value for the given timestamp, you should return the value associated with the largest timestamp less than or equal to the given one.
     unordered_map<string, vector<pair<string,int>>> mp;
     TimeMap() {
         
     }
-    
-    void set(string key, string value, int timestamp) {
+    // If L is the average length of key,value pair. Then inserting one pair would take L time(considering that the input is very long and rehashing is to be performed) and if we perform in total M operations than total timecomplexity will be ML
+    void set(string key, string value, int timestamp) { 
         mp[key].push_back({value, timestamp});
     }
     
+    // M at most elements-> BS -> LogM, To hash the string it takes L and if we perform N operations than the time complexity will be O(N L LogM).
     string get(string key, int timestamp) {
-        vector<pair<string,int>> searchableVector  = mp[key];
+        if (mp.find(key) == mp.end()) return "";
+        vector<pair<string,int>> &searchableVector  = mp[key];
         return binarySearch(searchableVector,timestamp);
 
     }
-    string binarySearch(vector<pair<string,int>> searchableVector, int timestamp){
+    string binarySearch(vector<pair<string,int>> &searchableVector, int timestamp){
         int low = 0;
         int high = searchableVector.size() - 1;
         int result = -1;
