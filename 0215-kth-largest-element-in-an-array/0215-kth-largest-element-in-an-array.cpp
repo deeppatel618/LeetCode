0b43cb4c9c2dd4c55360984f4daf7cbc5141d915
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2025-04-05 13:01:41 -0700

    Time: 12 ms (93.76%), Space: 78.7 MB (9.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,52 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
+    // Approach 1 - Easy
+    int findKthLargest2(vector<int>& nums, int k) {
         priority_queue<int> pq;
         int ans=-1;
         for(auto i: nums)
         {
-            pq.push(i);
+            pq.push(-i);
+            if(pq.size() > k)
+                pq.pop();
         }
-        for(int i =1;i<=k ;i++)
-        {
-            ans = pq.top();
-            pq.pop();
+        return -pq.top();
+    }
+
+
+
+
+    // Approach 2 - quick select
+    int findKthLargest(vector<int>& nums, int k) {
+        return quickSelect(nums,k);
+    }
+    int quickSelect(vector<int> &nums,int k){
+        // Select pivot at random.
+        int pivot = nums[rand() % nums.size()];
+
+        vector<int> left;
+        vector<int> mid;
+        vector<int> right;
+
+        for(int num:nums){
+            if(num > pivot){
+                left.push_back(num);
+            }
+            else if(num<pivot){
+                right.push_back(num);
+            }
+            else{
+                mid.push_back(num);
+            }
         }
-        return ans;
+
+        if(k <= left.size()){
+            return quickSelect(left,k);
+        }
+
+        if(left.size() + mid.size() < k){
+            return quickSelect(right, k - left.size() - mid.size());
+        }
+        return pivot;
     }
 };
