0cfc223b798141beab2ff8aac8d53addda982e0d
Deep Patel <66318740+deeppatel618@users.noreply.github.com>
2025-04-06 08:30:59 -0700

    Time: 79 ms (11.28%), Space: 69.6 MB (9.3%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    void combinationCalculator(int index, int sum, int target, vector<int> &nums,vector<vector<int>> &ans,vector<int> &temp)
+    // void combinationCalculator(int index, int sum, int target, vector<int> &nums,vector<vector<int>> &ans,vector<int> &temp)
+    // {
+    //     if(sum > target) return;
+    //     if(sum == target) {
+    //         ans.push_back(temp);
+    //         return;
+    //     }
+    //     for(int i = index; i < nums.size(); i++)
+    //     {
+    //         temp.push_back(nums[i]);
+    //         combinationCalculator(i,sum+nums[i],target,nums,ans,temp);
+    //         temp.pop_back();
+    //     }
+    // }
+
+    void combinationCalculator(int index, int target, vector<int> curr, vector<int> candidates, vector<vector<int>> &ans)
     {
-        if(sum > target) return;
-        if(sum == target) {
-            ans.push_back(temp);
+        if(target <= 0 || index == candidates.size())
+        {
+            if(target == 0)
+            {
+                ans.push_back(curr);
+            }
             return;
         }
-        for(int i = index; i < nums.size(); i++)
+        if(candidates[index] <= target)
         {
-            temp.push_back(nums[i]);
-            combinationCalculator(i,sum+nums[i],target,nums,ans,temp);
-            temp.pop_back();
+            curr.push_back(candidates[index]);
+            combinationCalculator(index, target - candidates[index], curr,candidates,ans);
+            curr.pop_back();
         }
+        combinationCalculator(index+1, target, curr,candidates,ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> resultTracker;
         vector<vector<int>> result;
-        combinationCalculator(0,0,target,candidates,result,resultTracker);
+        // combinationCalculator(0,0,target,candidates,result,resultTracker);
+        combinationCalculator(0,target,{},candidates,result);
         return result;
     }
 };
